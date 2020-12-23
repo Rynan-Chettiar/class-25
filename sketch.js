598fe226273ff7e2a81f1f8613ea5a0ed21d7206
Rynan-Chettiar <chettiar.rynan@gmail.com>
2020-12-23 20:55:00 +0530

    added platform in class26

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundimage;
+var platform;
 
 function preload()
 {
@@ -20,6 +21,8 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,305,300,170)
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -62,4 +65,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
